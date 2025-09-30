954cfdb198e0877c3985f953c9c5d7f453b20232
dsriva03 <dsriva03@gmail.com>
2025-09-30 13:33:49 -0500

    feat: create entrypoint for reasonsaver

---
+++ reasonsaver/src/cli.py
@@ -0,0 +1,31 @@
+#create an entrypoint for reason-saver
+import argparse
+from evaluate import main as evaluate_main
+
+def run():
+    parser = argparse.ArgumentParser(
+        description="ReasonSaver CLI: evaluate completions at scale"
+    )
+    parser.add_argument(
+        "--input",
+        type=str,
+        default="data/completions.json",
+        help="Path to completions.json file"
+
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default="data/evaluations.json",
+        help="Where to save evaluations"
+    )
+
+    args = parser.parse_args()
+
+    #call evaluate.py main function
+    evaluate_main(input_path=args.input, output_path=args.output)
+
+if __name__ == "__main__:
+    run()
+
+    
